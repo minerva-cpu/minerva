692d9be471647ddc8d87c2f6c5cbe37d7d26d9fc
Jean-François Nguyen <jf@lse.epita.fr>
2019-01-27 16:46:09 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup, find_packages
+
+
+if sys.version_info[:3] < (3, 6):
+    raise SystemExit("Minerva requires Python 3.6+")
+
+
+setup(
+    name="minerva",
+    version="0.1",
+    description="A 32-bit RISC-V soft processor",
+    author="Jean-François Nguyen",
+    author_email="jf@lambdaconcept.fr",
+    license="BSD",
+    install_requires=["nmigen"],
+    packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/lambdaconcept/minerva",
+        "Bug Tracker": "https://github.com/lambdaconcept/minerva/issues"
+    }
+)
