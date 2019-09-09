3553e42dbf8cf4c6b7f73f42a7f239f94d7be41f
Jean-François Nguyen <jf@lambdaconcept.com>
2019-09-09 19:47:50 +0200

    cache: Add L1Cache redesign and verification testbench.
    In an effort to simplify the cache design and minimize its impact on
    the pipeline, the "early restart" policy has been abandoned for now.

--- minerva/cache.py
+++ minerva/cache.py
@@ -2,11 +2,12 @@ from functools import reduce
 from operator import or_
 
 from nmigen import *
+from nmigen.asserts import *
 from nmigen.lib.coding import Encoder
 from nmigen.tools import log2_int
 
 
-__all__ = ["L1Cache"]
+__all__ = ["L1Cache", "L1CacheV2"]
 
 
 def split(v, *counts):
@@ -295,5 +296,140 @@ class L1Cache(Elaboratable):
                 m.d.comb += way.data.eq(data_rp.data.part(s2_offset*32, 32))
             with m.Else():
                 m.d.comb += way.data.eq(latch_data_rp_data.part(s2_offset*32, 32))
+
+        return m
+
+
+class L1CacheV2(Elaboratable):
+    def __init__(self, nways, nlines, nwords, base, limit):
+        if not nlines or nlines & nlines-1:
+            raise ValueError("nlines must be a power of 2, not {!r}".format(nlines))
+        if nwords not in {4, 8, 16}:
+            raise ValueError("nlines must be a power of 2, not {!r}".format(nwords))
+        if nways not in {1, 2}:
+            raise ValueError("nways must be 1 or 2, not {!r}".format(nways))
+
+        self.nways = nways
+        self.nlines = nlines
+        self.nwords = nwords
+        self.base = base
+        self.limit = limit
+
+        offsetbits = log2_int(nwords)
+        linebits = log2_int(nlines)
+        tagbits = log2_int(limit-base) - log2_int(nlines) - log2_int(nwords) - 2
+
+        self.s1_addr = Record([("offset", offsetbits), ("line", linebits), ("tag", tagbits)])
+        self.s1_flush = Signal()
+        self.s1_stall = Signal()
+        self.s1_valid = Signal()
+        self.s2_addr = Record.like(self.s1_addr)
+        self.s2_re = Signal()
+        self.s2_evict = Signal()
+        self.s2_valid = Signal()
+        self.bus_valid = Signal()
+        self.bus_error = Signal()
+        self.bus_rdata = Signal(32)
+
+        self.s2_miss = Signal()
+        self.s2_rdata = Signal(32)
+        self.bus_re = Signal()
+        self.bus_addr = Record.like(self.s1_addr)
+        self.bus_last = Signal()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        ways = Array(Record([("data",   self.nwords * 32),
+                             ("tag",    self.s2_addr.tag.shape()),
+                             ("valid",  1),
+                             ("bus_re", 1)])
+                     for _ in range(self.nways))
+
+        if self.nways == 1:
+            way_lru = Const(0)
+        elif self.nways == 2:
+            way_lru = Signal()
+            with m.If(self.bus_re & self.bus_valid & self.bus_last & ~self.bus_error):
+                m.d.sync += way_lru.eq(~way_lru)
+
+        m.d.comb += ways[way_lru].bus_re.eq(self.bus_re)
+
+        way_hit = m.submodules.way_hit = Encoder(self.nways)
+        for j, way in enumerate(ways):
+            m.d.comb += way_hit.i[j].eq((way.tag == self.s2_addr.tag) & way.valid)
+
+        m.d.comb += [
+            self.s2_miss.eq(way_hit.n),
+            self.s2_rdata.eq(ways[way_hit.o].data.word_select(self.s2_addr.offset, 32))
+        ]
+
+        with m.FSM() as fsm:
+            last_offset = Signal.like(self.s2_addr.offset)
+
+            with m.State("CHECK"):
+                with m.If(self.s2_re & self.s2_miss & self.s2_valid):
+                    m.d.sync += [
+                        self.bus_addr.eq(self.s2_addr),
+                        self.bus_re.eq(1),
+                        last_offset.eq(self.s2_addr.offset - 1)
+                    ]
+                    m.next = "REFILL"
+
+            with m.State("REFILL"):
+                m.d.comb += self.bus_last.eq(self.bus_addr.offset == last_offset)
+                with m.If(self.bus_valid):
+                    m.d.sync += self.bus_addr.offset.eq(self.bus_addr.offset + 1)
+                with m.If(self.bus_valid & self.bus_last | self.bus_error):
+                    m.d.sync += self.bus_re.eq(0)
+                with m.If(~self.bus_re & ~self.s1_stall):
+                    m.next = "CHECK"
+
+        if platform == "formal":
+            with m.If(Initial()):
+                m.d.comb += Assume(fsm.ongoing("CHECK"))
+
+        for way in ways:
+            valid_lines = Signal(self.nlines)
+
+            with m.If(self.s1_flush & self.s1_valid):
+                m.d.sync += valid_lines.eq(0)
+            with m.Elif(way.bus_re & self.bus_error):
+                m.d.sync += valid_lines.bit_select(self.bus_addr.line, 1).eq(0)
+            with m.Elif(way.bus_re & self.bus_valid & self.bus_last):
+                m.d.sync += valid_lines.bit_select(self.bus_addr.line, 1).eq(1)
+            with m.Elif(self.s2_evict & self.s2_valid & (way.tag == self.s2_addr.tag)):
+                m.d.sync += valid_lines.bit_select(self.s2_addr.line, 1).eq(0)
+
+            tag_mem = Memory(width=len(way.tag), depth=self.nlines)
+            tag_rp = tag_mem.read_port()
+            tag_wp = tag_mem.write_port()
+            m.submodules += tag_rp, tag_wp
+
+            data_mem = Memory(width=len(way.data), depth=self.nlines)
+            data_rp = data_mem.read_port()
+            data_wp = data_mem.write_port(granularity=32)
+            m.submodules += data_rp, data_wp
+
+            m.d.comb += [
+                tag_rp.addr.eq(Mux(self.s1_stall, self.s2_addr.line, self.s1_addr.line)),
+                data_rp.addr.eq(Mux(self.s1_stall, self.s2_addr.line, self.s1_addr.line)),
+
+                tag_wp.addr.eq(self.bus_addr.line),
+                tag_wp.en.eq(way.bus_re & self.bus_valid & self.bus_last),
+                tag_wp.data.eq(self.bus_addr.tag),
+
+                data_wp.addr.eq(self.bus_addr.line),
+                data_wp.en.bit_select(self.bus_addr.offset, 1).eq(way.bus_re & self.bus_valid),
+                data_wp.data.eq(self.bus_rdata << self.bus_addr.offset*32),
+
+                way.valid.eq(valid_lines.bit_select(self.s2_addr.line, 1)),
+                way.tag.eq(tag_rp.data),
+                way.data.eq(data_rp.data)
+            ]
+
+            if platform == "formal":
+                with m.If(Initial()):
+                    m.d.comb += Assume(~valid_lines.bool())
 
         return m
---
+++ minerva/test/test_cache.py
@@ -0,0 +1,91 @@
+from nmigen import *
+from nmigen.tools import log2_int
+from nmigen.test.tools import *
+from nmigen.asserts import *
+
+from ..cache import L1CacheV2 as L1Cache
+
+
+class L1CacheSpec(Elaboratable):
+    """
+    A cache read at a given address must either:
+    - miss and start a cache refill;
+    - hit and return the last refilled value for that address.
+    """
+    def __init__(self, cache):
+        self.cache = cache
+
+    def elaborate(self, platform):
+        m = Module()
+        m.submodules.dut = cache = self.cache
+
+        addr  = AnyConst(cache.s1_addr.shape())
+        s1_re = AnySeq(1)
+
+        with m.If(~cache.s1_stall):
+            m.d.sync += [
+                cache.s2_addr.eq(cache.s1_addr),
+                cache.s2_re.eq(s1_re),
+                cache.s2_valid.eq(cache.s1_valid)
+            ]
+
+        m.d.comb += Assume(cache.s1_valid.implies(cache.s1_addr == addr))
+        with m.If(cache.s2_re & cache.s2_miss & cache.s2_valid):
+            m.d.comb += Assume(cache.s1_stall)
+
+        spec_s2_rdata = Signal(32)
+        spec_bus_re   = Signal()
+        spec_bus_addr = Record.like(cache.bus_addr)
+        spec_bus_last = Signal()
+        last_offset   = Signal.like(cache.s2_addr.offset)
+
+        with m.If(~Past(cache.s1_stall)):
+            with m.If(cache.s2_re & cache.s2_miss & cache.s2_valid):
+                m.d.sync += [
+                    spec_bus_re.eq(1),
+                    spec_bus_addr.eq(cache.s2_addr),
+                    last_offset.eq(cache.s2_addr.offset - 1)
+                ]
+
+        with m.If(cache.bus_re):
+            with m.If(cache.bus_valid):
+                # Lines are refilled with an incremental burst that starts at the missed address
+                # and wraps around the offset bits.
+                m.d.sync += spec_bus_addr.offset.eq(spec_bus_addr.offset + 1)
+                with m.If((cache.bus_addr == cache.s2_addr) & ~cache.bus_error):
+                    m.d.sync += spec_s2_rdata.eq(cache.bus_rdata)
+            # A burst ends when all words in the line have been refilled, or an error occured.
+            m.d.comb += spec_bus_last.eq(cache.bus_addr.offset == last_offset)
+            with m.If(cache.bus_valid & cache.bus_last | cache.bus_error):
+                m.d.sync += spec_bus_re.eq(0)
+
+        with m.If(cache.s2_re & cache.s2_valid & ~cache.s2_miss):
+            m.d.comb += Assert(cache.s2_rdata == spec_s2_rdata)
+
+        m.d.comb += Assert(cache.bus_re == spec_bus_re)
+        with m.If(cache.bus_re):
+            m.d.comb += [
+                Assert(cache.bus_addr == spec_bus_addr),
+                Assert(cache.bus_last == spec_bus_last)
+            ]
+
+        with m.If(Initial()):
+            m.d.comb += [
+                Assume(cache.s1_stall),
+                Assume(~cache.s2_valid),
+                Assume(~cache.bus_re),
+                Assume(~spec_bus_re)
+            ]
+
+        return m
+
+
+class L1CacheTestCase(FHDLTestCase):
+    def check(self, cache):
+        self.assertFormal(L1CacheSpec(cache), mode="bmc", depth=10)
+
+    def test_direct_mapped(self):
+        self.check(L1Cache(nways=1, nlines=2, nwords=4, base=0, limit=1024))
+
+    def test_2_ways(self):
+        self.check(L1Cache(nways=2, nlines=2, nwords=4, base=0, limit=1024))
