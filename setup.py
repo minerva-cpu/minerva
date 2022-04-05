f1388d826122b150c09ed6c58ccf081e1a2b3e85
Jean-François Nguyen <jf@jfng.fr>
2022-04-05 14:26:01 +0200

    Update copyright notice and project URL.

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(
     version="0.1",
     description="A 32-bit RISC-V soft processor",
     author="Jean-François Nguyen",
-    author_email="jf@lambdaconcept.com",
+    author_email="jf@jfng.fr",
     license="BSD",
     python_requires=">=3.6",
     install_requires=["amaranth>=0.2,<0.5"],
     extras_require={ "debug": ["jtagtap"] },
     packages=find_packages(),
     project_urls={
-        "Source Code": "https://github.com/lambdaconcept/minerva",
-        "Bug Tracker": "https://github.com/lambdaconcept/minerva/issues"
+        "Source Code": "https://github.com/minerva-cpu/minerva",
+        "Bug Tracker": "https://github.com/minerva-cpu/minerva/issues"
     }
 )
