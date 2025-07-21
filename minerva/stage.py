e505d7edf1dfdebae4482cc14c00cf29f472f83c
Ahmed Charles <me@ahmedcharles.com>
2025-07-21 18:35:13 +0000

    Add some comments to the Stage class.

--- minerva/stage.py
+++ minerva/stage.py
@@ -7,6 +7,33 @@ __all__ = ["Stage"]
 
 
 class Stage(wiring.Component):
+    """Pipeline stage component.
+
+    Arguments
+    ---------
+    sink_layout : :class:`Layout`
+        Data received from the previous pipeline stage, if any.
+    source_layout : :class:`Layout`
+        Data sent to the next pipeline stage, if any.
+    sink_init
+        Initial value of the payload data received from the previous pipeline
+        stage.
+    source_init
+        Initial value of the payload data sent to the next pipeline stage.
+
+    Members
+    -------
+    valid : ``Out(1)``
+        High when the previous pipeline stage, if any, was not 'killed' last
+        cycle and this stage is not 'killed' this cycle.
+    ready : ``Out(1)``
+        High when the next pipeline stage, if any, is not stalled this cycle and
+        this stage is not 'stalled' this cycle.
+    sink : ``In(stream.Signature(sink_layout, payload_init=sink_init))``
+        Stream connection to the previous pipeline stage.
+    source : ``Out(stream.Signature(source_layout, payload_init=source_init))``
+        Stream connection to the next pipeline stage.
+    """
     def __init__(self, sink_layout, source_layout, *, sink_init=None, source_init=None):
         self._kill_any  = 0
         self._stall_any = 0
